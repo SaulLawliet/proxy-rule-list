08ad70804173799dc3fb6f9021f3bee2d5024c16
Saul Lawliet <october.sunbathe@gmail.com>
2015-09-03 21:34:28 +0800

    nothing to say

--- sort-and-push.sh
+++ sort-and-push.sh
@@ -2,7 +2,7 @@
 
 file_name="list.txt"
 sed '/\[AutoProxy]/d;/^$/d' $file_name |LC_ALL=c sort -u -o $file_name
-if [[ "0" == `cat list.txt |wc -l` ]]; then
+if [[ "0" == $(cat $file_name |wc -l) ]]; then
     echo -e "[AutoProxy]\n" > $file_name
 else
     sed -i '1i [AutoProxy]\n' $file_name
