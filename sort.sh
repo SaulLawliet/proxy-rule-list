1d970f50f812ad5415953ef44deb320dc1b77a69
SaulLawliet <october.sunbathe@gmail.com>
2020-06-03 23:17:08 +0800

    TODO: Fix later

--- sort.sh
+++ sort.sh
@@ -1,9 +1,12 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+sed="sed"
+[[ $(uname) == "Darwin" ]] && sed="gsed"
 
 file_name="list.txt"
-sed '/\[AutoProxy]/d;/^$/d' $file_name |LC_ALL=c sort -u -o $file_name
+$sed '/\[AutoProxy]/d;/^$/d' $file_name |LC_ALL=c sort -u -o $file_name
 if [[ "0" == $(cat $file_name |wc -l) ]]; then
     echo -e "[AutoProxy]\n" > $file_name
 else
-    sed -i '1i [AutoProxy]\n' $file_name
+    $sed -i '1i [AutoProxy]\n' $file_name
 fi
